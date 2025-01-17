1d16b759c4a7ee8388f020b0eb3baaf8a909d968
Kirn1465313 <kiransahukar16@gmail.com>
2025-01-17 19:26:35 +0530

    Create january17.py

---
+++ january2025/january17.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def productExceptSelf(self, arr):
+        #code here
+        product,zero=1,arr.count(0)
+        if zero>1:
+            return [0]*len(arr)
+        for x in arr:
+            if x!=0:
+                product*=x
+        return [product if x==0 else 0 if zero else product // x for x in arr]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for _ in range(t):
+
+        arr = [int(x) for x in input().split()]
+
+        ans = Solution().productExceptSelf(arr)
+        print(*ans)
+        print("~")
+
+# } Driver Code Ends
