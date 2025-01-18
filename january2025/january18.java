0c3e58df4241b2b2dc96b5c0ce8502f932fb9c27
Kirn1465313 <kiransahukar16@gmail.com>
2025-01-18 15:33:27 +0530

    Create january18.java

---
+++ january2025/january18.java
@@ -0,0 +1,22 @@
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
