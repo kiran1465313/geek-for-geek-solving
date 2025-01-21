eaf079fc0e231e1ed74fdb145e36504065d96e55
Kirn1465313 <kiransahukar16@gmail.com>
2025-01-21 20:15:43 +0530

    Create january21.py

---
+++ january2025/january21.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reverseKGroup(self, head, k):
+        l = []
+        i = 0
+        t = []
+        while head:
+            t.append(head.data)
+            head = head.next
+            i += 1
+            if i == k or not head:
+                l.append(t)
+                t = []
+                i = 0
+        n = []
+        for i in range(len(l)):
+            n.extend(reversed(l[i]))
+        # print(n)
+        
+        new = Node(n[0])
+        current = new
+        for i in range(1,len(n)):
+            t = Node(n[i])
+            current.next = t
+            current = current.next
+        
+        return new
